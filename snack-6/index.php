d550418009f4e96ccd61255f07fcb43c7099a20f
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-04-21 17:50:38 +0200

    correzione

--- snack-6/index.php
+++ snack-6/index.php
@@ -46,7 +46,7 @@ Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM
         <?php  for ($i=0; $i < count($Keys) ; $i++) { ?>
                 <div class="<?php if ($Keys[$i] == 'teachers') { echo 'green'; } else { echo 'gray'; } ?>">
                     <ol>
-                        u<?php for ($index=0; $index < count($db[$Keys[$i]]) ; $index++) { ?>
+                        <?php for ($index=0; $index < count($db[$Keys[$i]]) ; $index++) { ?>
                         <li> 
                             <?php echo $db[$Keys[$i]][$index]["name"] . " " . $db[$Keys[$i]][$index]["lastname"];}  ?> 
                         </li>
